32cefa9c45796f4b5fa5f7c9b3788dbf6ec465ab
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-02-09 21:10:38 +0900

    New: all done

---
+++ css/action.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * op-layout-uqunie.com:/css/action.php
+ *
+ * @creation  2017-08-11
+ * @version   1.0
+ * @package   op-layout-uqunie.com
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+return [
+	__DIR__.'/body',
+	__DIR__.'/color',
+	__DIR__.'/header',
+	__DIR__.'/footer',
+	__DIR__.'/container',
+];
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * op-layout-uqunie.com:/index.php
+ *
+ * @creation  2017-08-04
+ * @version   1.0
+ * @package   op-layout-uqunie.com
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+include('html.phtml');
