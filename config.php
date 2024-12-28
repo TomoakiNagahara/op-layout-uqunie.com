2de01a70cc87900960ca0f84c68d7bdfd83fec4e
debian12.uqunie.com <1668339+TomoakiNagahara@users.noreply.github.com>
2024-12-28 11:52:09 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/** op-layout-uqunie.com:/config.php
+ *
+ * @creation   2024-12-28
+ * @version    1.0
+ * @package    op-layout-uqunie.com
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+return [];
