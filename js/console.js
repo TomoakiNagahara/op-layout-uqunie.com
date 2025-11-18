bbfcf2c9d310c7d248277b711d5de78ddf9b0d32
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-18 18:15:41 +0900

    New: js > console.js

---
+++ js/console.js
@@ -0,0 +1,11 @@
+
+/**	op-layout-uqunie.com:/js/console.js
+ *
+ * @creation  2025-11-18
+ * @version   1.0
+ * @package   op-layout-uqunie.com
+ * @author    Tomoaki Nagahara
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+console.log('uqunie.com');
